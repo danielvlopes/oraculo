ffef2347f51c28f275defe064d24cf220815fd4b
Rondy <rondysousa@gmail.com>
2011-07-05 19:06:02 -0300

    Adicionado o modelo Image

---
+++ db/migrate/20110705215745_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration
+  def self.up
+    create_table :images do |t|
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :images
+  end
+end
