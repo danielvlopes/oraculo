4cbdd66ce6516abf91e285e856bdbdf1f9798aa3
Juarez Lustosa <juarez.lustosa@gmail.com>
2011-07-17 16:04:08 -0300

    Modelo com scopes para realização da pesquisa

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,16 @@ class Question < ActiveRecord::Base
 
   validates_presence_of :title, :body,:owner
   validates_associated :owner
+
+  scope :search_title, lambda { |terms|
+    where("title LIKE :t OR body LIKE :t", t: "%#{terms}%")
+  }
+
+  scope :search_author, lambda { |terms|
+    joins(:owner).where("users.name LIKE :n OR body LIKE :n", n: "%#{terms}%")
+  }
+
+  scope :search_date, lambda { |start_at, end_at|
+    where("questions.created_at >= ? AND questions.created_at <= ? ", "#{start_at} 00:00:00", "#{end_at} 23:59:99" )
+  }
 end
