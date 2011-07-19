7dd689559de06e05cb6b8f65ab8fa49dbd50cb52
marthaluiza <mlxisto@yahoo.com.br>
2011-07-18 19:10:09 -0700

    Edited app/models/question.rb via GitHub

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :owner, :class_name => "User", :foreign_key => "user_id"
+  captcha :nickname
 
   validates_presence_of :title, :body,:owner
   validates_associated :owner
